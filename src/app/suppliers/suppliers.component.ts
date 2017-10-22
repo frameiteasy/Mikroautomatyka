f56d7c84959d83c5363f2b2c858bcf163680a759
Konrad Marciniak <konmar@tlen.pl>
2017-10-22 11:58:33 +0200

    drobne poprawki

--- src/app/suppliers/suppliers.component.ts
+++ src/app/suppliers/suppliers.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SuppliersService } from '../services/suppliers.service';
 
 @Component({
   selector: 'app-suppliers',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SuppliersComponent implements OnInit {
 
-  constructor() { }
+  constructor(private suppliersService: SuppliersService) { }
 
   ngOnInit() {
   }
