49159c5be78f237cbd095c75c3eb4a3eb87024eb
Konrad Marciniak <konmar@tlen.pl>
2017-10-22 12:00:18 +0200

    drobne poprawki

---
+++ src/app/suppliers/supplier-search/supplier-search.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-supplier-search',
+  templateUrl: './supplier-search.component.html',
+  styleUrls: ['./supplier-search.component.css']
+})
+export class SupplierSearchComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
