8b319b9073049340aebf2452bcb043b346ff1e4d
Konrad Marciniak <konmar@tlen.pl>
2017-11-04 13:22:08 +0100

    Components responsible for pick and place presentation

---
+++ src/app/project/pick-and-place/pick-and-place.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pick-and-place',
+  templateUrl: './pick-and-place.component.html',
+  styleUrls: ['./pick-and-place.component.css']
+})
+export class PickAndPlaceComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
