4ae0a8b0bc14bf28ae653dd54c583041d2e8d98e
Konrad Marciniak <konmar@tlen.pl>
2017-11-04 13:12:13 +0100

    Elements list in a project

---
+++ src/app/project/elements-in-project/elements-in-project.component.ts
@@ -0,0 +1,43 @@
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import { ElementInProject } from '../../models/element-in-project.model';
+import { ActivatedRoute } from '@angular/router';
+import { CustomersService } from '../../services/customers.service';
+import { ElementsService } from '../../services/elements.service';
+
+@Component({
+  selector: 'app-components-in-project',
+  templateUrl: './elements-in-project.component.html',
+  styleUrls: ['./elements-in-project.component.css']
+})
+export class ElementsInProjectComponent implements OnInit, OnDestroy {
+  elements: ElementInProject[];
+  filteringString = '';
+
+  constructor(private route: ActivatedRoute,
+              private customerService: CustomersService,
+              private elementService: ElementsService) { }
+
+  ngOnInit() {
+    console.log('ElementsInProjectComponent.ngOnInit');
+    console.log(this.route.snapshot.parent.params);
+    const customerID = this.route.snapshot.parent.params['idCustomer'];
+    const projectID = this.route.snapshot.parent.params['idProject'];
+    const project = this.customerService.getCustomers()[customerID].getProjects()[projectID];
+    this.elements = this.customerService.getCustomers()[customerID].getProjects()[projectID].getElements();
+
+    console.log(project);
+    // this.elements = this.elementsService.getElements();
+    this.elementService.filterElementsChanged
+      .subscribe(
+        (filter: string) => {
+          this.filteringString = filter;
+        }
+      )
+
+  }
+
+  ngOnDestroy() {
+    this.elementService.filterElementsChanged.unsubscribe();
+  }
+
+}
