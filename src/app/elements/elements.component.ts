cfddf224387d0c30210ac3f1d5cc35eee97bbb19
Konrad Marciniak <konmar@tlen.pl>
2017-11-04 13:36:06 +0100

    Components responsible for the elements dictionary

---
+++ src/app/elements/elements.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-components',
+  templateUrl: './elements.component.html',
+  styleUrls: ['./elements.component.css']
+})
+export class ElementsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
