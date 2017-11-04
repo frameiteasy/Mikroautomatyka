bf0863cfe3a497ebd1c69eab88756006b55654e6
Konrad Marciniak <konmar@tlen.pl>
2017-11-04 13:28:39 +0100

    add getElements() method

--- src/app/models/project.model.ts
+++ src/app/models/project.model.ts
@@ -1,21 +1,24 @@
 import { Status } from './status.model';
+import { ElementInProject } from './element-in-project.model';
 
 export class Project {
 
   private id: number;
   private name: string;
   private status: Status;
-  private bom: string;
-  private pandp: string;
+  private bomFile: string;
+  private pandpFile: string;
   private customerId: number;
+  private elements: ElementInProject[];
 
   constructor(id: number, name: string, status: Status, bom: string, pandp: string, customerId: number) {
     this.id = id;
     this.name = name;
     this.status = status;
-    this.bom = bom;
-    this.pandp = pandp;
+    this.bomFile = bom;
+    this.pandpFile = pandp;
     this.customerId = customerId;
+    this.elements = [];
   }
 
   getId(): number {
@@ -28,6 +31,17 @@ export class Project {
 
   public getStatus(): Status {
     return this.status;
+  }
+
+  public getElements(): ElementInProject[] {
+    if (!this.elements) {
+      this.elements = [];
+    }
+    return this.elements;
+  }
+
+  public addElement(element: ElementInProject) {
+    this.elements.push(element);
   }
 
 }
