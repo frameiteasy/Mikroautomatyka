e9f56afdc26e16c415fc911109df3de69db83275
Konrad Marciniak <konmar@tlen.pl>
2017-11-04 13:29:11 +0100

    fix model

--- src/app/models/element.model.ts
+++ src/app/models/element.model.ts
@@ -1,51 +1,34 @@
-import {ElementName} from './element-name.model';
+import { ElementName } from './element-name.model';
 
 export class Element {
   id: number;
   name: ElementName;
   description: string;
-  amount: number;
   limit: number;
-  loss: number;
-  warehousesIds: number[];
-  suppliersIds: number[];
+  slop: number;
 
   constructor(id: number,
               name: ElementName,
               desc: string,
-              amount: number,
               limit: number,
-              loss: number,
-              warehousesIds: number[],
-              suppliersIds: number[]) {
+              slop: number) {
     this.id = id;
     this.name = name;
     this.description = desc;
-    this.amount = amount;
     this.limit = limit;
-    this.loss = loss;
-    this.warehousesIds = warehousesIds;
-    this.suppliersIds = suppliersIds;
+    this.slop = slop;
   }
 
-  public getElementName(): string {
-    return this.name.getName();
-  }
-
-  public getAmount(): number {
-    return this.amount;
+  public getName(): ElementName {
+    return this.name;
   }
 
   public getLimit(): number {
     return this.limit;
   }
 
-  public getLoss(): number {
-    return this.loss;
-  }
-
-  public getWarehouseName() {
-    return 'Magazyn';
+  public getSlop(): number {
+    return this.slop;
   }
 
 }
