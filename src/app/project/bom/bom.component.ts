2166d0ad8b5ba433b6b57b346b90cddd16b3e013
Konrad Marciniak <konmar@tlen.pl>
2017-11-05 11:39:02 +0100

    bom handling

---
+++ src/app/project/bom/bom.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bom',
+  templateUrl: './bom.component.html',
+  styleUrls: ['./bom.component.css']
+})
+export class BomComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
