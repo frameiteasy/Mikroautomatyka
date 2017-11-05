e073cc260dd135205f17a2a4dcf64bfda2f489b0
Konrad Marciniak <konmar@tlen.pl>
2017-11-05 11:40:14 +0100

    minor changes

--- src/app/projects/projects-list/project-list-item/project-list-item.component.ts
+++ src/app/projects/projects-list/project-list-item/project-list-item.component.ts
@@ -1,29 +1,30 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Project } from '../../../models/project.model';
+import { ActivatedRoute, Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-project-list-item',
   templateUrl: './project-list-item.component.html',
   styleUrls: ['./project-list-item.component.css']
 })
+
 export class ProjectListItemComponent implements OnInit {
   @Input() project: Project;
   @Input() index: number;
+  customerIndex: number;
+  projectIndex: number;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute,
+              private router: Router) { }
 
   ngOnInit() {
+    this.customerIndex = this.route.snapshot.parent.params['id'];
+    this.projectIndex = this.index;
   }
 
-  onEdit() {
-
+  onView() {
+    this.router.navigate(['/project', this.customerIndex, this.projectIndex]);
   }
 
-  onViewBOM() {
-
-  }
-
-  onViewPandP() {
-
-  }
 }
