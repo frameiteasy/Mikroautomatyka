fc83dc01929a60e39f079df364c89bb35736e7dd
Konrad Marciniak <konmar@tlen.pl>
2017-11-05 11:38:07 +0100

    elements filtering

---
+++ src/app/elements/elements-search/elements-search.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-components-search',
+  templateUrl: './elements-search.component.html',
+  styleUrls: ['./elements-search.component.css']
+})
+export class ElementsSearchComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
