1639f9dfc4b66cc407cb2b85001293c4c5f5cd4c
Konrad Marciniak <konmar@tlen.pl>
2017-11-05 12:03:51 +0100

    load test data components

---
+++ src/app/load-test-data/load-test-data.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-load-test-data',
+  templateUrl: './load-test-data.component.html',
+  styleUrls: ['./load-test-data.component.css']
+})
+export class LoadTestDataComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
