d7ae80248a3954a8bcae6f068d7ac0f3775cd905
Konrad Marciniak <konmar@tlen.pl>
2017-11-12 12:31:47 +0100

    change the way of filtering elements

--- src/app/elements/elements-list/elements-list.pipe.ts
+++ src/app/elements/elements-list/elements-list.pipe.ts
@@ -9,12 +9,12 @@ export class ElementsListPipe implements PipeTransform {
   filterString = '';
 
   transform(value: Element[], filterString: string): any {
-    if (value.length === 0 || filterString === '') {
+    if (value.length === 0 || filterString === '' || filterString.length < 2) {
       return value;
     }
     const resultArray = [];
     for (const item of value) {
-      if (item.getName().getName().toLowerCase().startsWith(filterString.toLowerCase())) {
+      if (item.getName().getName().toLowerCase().indexOf(filterString.toLowerCase()) !== -1) {
         resultArray.push(item);
       }
     }
